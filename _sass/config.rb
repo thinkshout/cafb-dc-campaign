09eb61be63839faa18bba5170d53534ba29768d2
Eric Paxton <heypaxton@gmail.com>
2014-06-18 11:53:37 -0700

    Add asset config

--- _sass/config.rb
+++ _sass/config.rb
@@ -5,9 +5,9 @@ require 'susy'
 # Set this to the root of your project when deployed:
 http_path = "/"
 sass_dir = "./"
-css_dir = "../stylesheets"
-images_dir = "../images"
-javascripts_dir = "../javascripts"
+css_dir = "../css"
+images_dir = "../img"
+javascripts_dir = "../js"
 relative_assets = true
 
 # You can select your preferred output style here (can be overridden via the command line):
