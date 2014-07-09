b418fd9577e374c38f781bbe70b6bd67023a9760
Eric Paxton <heypaxton@gmail.com>
2014-07-09 16:10:34 -0700

    Add fundraiser and login pages

--- js/app.js
+++ js/app.js
@@ -7,6 +7,20 @@ $(document).ready(function(){
   $('#no-end').click(function(){
     $('#campaign-dates').fadeToggle();  
   });
+
+  $('#team-form, #indy-form').hide();
+  $('#team, #indy').click(function(){
+    var forms = '#' + $(this).attr('id') + '-form';
+    formToShow = $(forms);
+    if (forms == '#indy-form'){
+      formToShow.fadeIn(); 
+      $('#team-form').hide();
+    } else {
+      formToShow.fadeIn(); 
+      $('#indy-form').hide();
+    }
+     
+  });
 });
 
 $(document).foundation();
