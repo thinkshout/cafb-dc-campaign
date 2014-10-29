3d6220deb4c538d2c0ac0dd904114001564b59de
Gabe Carleton-Barnes <gabe@thinkshout.com>
2014-10-29 15:10:44 -0700

    Configuring pantheon deployment repo.

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # match your install profile name
-PROJECT=cafb_dc
+PROJECT=redhen_raiser
 
 # pantheon, acquia, generic
 HOSTTYPE=pantheon
 
 SITENAME="Capital Area Food Bank"
 
-GITREPO=
+GITREPO="ssh://codeserver.dev.f66e82b1-673b-4c39-bc8f-97262080757f@codeserver.dev.f66e82b1-673b-4c39-bc8f-97262080757f.drush.in:2222/~/repository.git"
