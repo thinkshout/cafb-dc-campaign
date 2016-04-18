9312190f06284fa73989570eb8c58036d2d976c9
Mike McCaffrey <mike.mccaffrey@thinkshout.com>
2016-04-18 13:36:54 -0700

    Don't redirect pantheon multidev sites to ssl

--- settings/site.settings.php
+++ settings/site.settings.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /**
  * @file site.settings.php
@@ -9,15 +9,18 @@ $conf['install_profile'] = 'redhen_raiser';
 
 // Require SSL.
 if (isset($_SERVER['PANTHEON_ENVIRONMENT'])) {
-  if (!isset($_SERVER['HTTP_X_SSL']) ||
-    (isset($_SERVER['HTTP_X_SSL']) && $_SERVER['HTTP_X_SSL'] != 'ON')) {
-       $dests = array(
+  if (!isset($_SERVER['HTTP_X_SSL']) || (isset($_SERVER['HTTP_X_SSL']) && $_SERVER['HTTP_X_SSL'] != 'ON')) {
+    // The pantheon environments that allow ssl connections
+    $dests = array(
       'live' => 'give.capitalareafoodbank.org',
       'test' => 'test-dc-campaign.pantheon.io',
       'dev' => 'dev-dc-campaign.pantheon.io',
     );
-    header('HTTP/1.0 301 Moved Permanently');
-    header('Location: https://' . $dests[$_SERVER['PANTHEON_ENVIRONMENT']] . $_SERVER['REQUEST_URI']);
-    exit();
+    // Only redirect to ssl if it is one of the environments that supports it
+    if(isset($dests[$_SERVER['PANTHEON_ENVIRONMENT']])) {
+     header('HTTP/1.0 301 Moved Permanently');
+      header('Location: https://' . $dests[$_SERVER['PANTHEON_ENVIRONMENT']] . $_SERVER['REQUEST_URI']);
+      exit();
+    }
   }
 }
