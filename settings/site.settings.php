cb434cd5cfe1f784d116e6f30e319bb8dd96d9b3
Mike McCaffrey <mike.mccaffrey@thinkshout.com>
2016-04-18 14:29:45 -0700

    Require SSL for all pantheon environments

--- settings/site.settings.php
+++ settings/site.settings.php
@@ -7,20 +7,11 @@
 
 $conf['install_profile'] = 'redhen_raiser';
 
-// Require SSL.
+// Require SSL for all pantheon environments
 if (isset($_SERVER['PANTHEON_ENVIRONMENT'])) {
   if (!isset($_SERVER['HTTP_X_SSL']) || (isset($_SERVER['HTTP_X_SSL']) && $_SERVER['HTTP_X_SSL'] != 'ON')) {
-    // The pantheon environments that allow ssl connections
-    $dests = array(
-      'live' => 'give.capitalareafoodbank.org',
-      'test' => 'test-dc-campaign.pantheon.io',
-      'dev' => 'dev-dc-campaign.pantheon.io',
-    );
-    // Only redirect to ssl if it is one of the environments that supports it
-    if(isset($dests[$_SERVER['PANTHEON_ENVIRONMENT']])) {
-     header('HTTP/1.0 301 Moved Permanently');
-      header('Location: https://' . $dests[$_SERVER['PANTHEON_ENVIRONMENT']] . $_SERVER['REQUEST_URI']);
-      exit();
-    }
+    header('HTTP/1.0 301 Moved Permanently');
+    header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+    exit();
   }
 }
