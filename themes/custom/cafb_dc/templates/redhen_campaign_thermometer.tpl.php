7dee71e8f327e4c8ba046302a0ca08160e8265d1
jami.dwyer@thinkshout.com <jami.dwyer@thinkshout.com>
2016-07-01 10:08:15 -0700

    template override for thermometer

---
+++ themes/custom/cafb_dc/templates/redhen_campaign_thermometer.tpl.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @file
+ * Default theme implementation for thermometer displays.
+ *
+ * @see template_preprocess_redhen_campaign_thermometer()
+ */
+?>
+<?php if (!($is_front)): /*&& use_thermometer */ ?>
+  <style type="text/css">
+    .redhen-campaign-thermo-percent-bar-wrapper .redhen-campaign-thermo-percent-bar {background: red;}
+  </style>
+<?php else: ?>
+  <style type="text/css">
+    .redhen-campaign-thermo-percent-<?php print $id; ?> { width: <?php print $percent; ?>%; }
+  </style>
+<?php endif; ?>
+<div class="redhen-campaign-thermo-percent-bar-wrapper">
+  <div class="redhen-campaign-thermo-percent-bar redhen-campaign-thermo-percent-<?php print $id; ?>"></div>
+</div>
