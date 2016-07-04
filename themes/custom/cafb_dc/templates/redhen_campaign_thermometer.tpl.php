c041f7b6e11afc72af9d32c7e88a813cd897d511
jami.dwyer@thinkshout.com <jami.dwyer@thinkshout.com>
2016-07-04 11:54:42 -0700

    adds custom-thermometer class to entity if it should use one

--- themes/custom/cafb_dc/templates/redhen_campaign_thermometer.tpl.php
+++ themes/custom/cafb_dc/templates/redhen_campaign_thermometer.tpl.php
@@ -6,15 +6,10 @@
  * @see template_preprocess_redhen_campaign_thermometer()
  */
 ?>
-<?php if (!($is_front)): /*&& use_thermometer */ ?>
   <style type="text/css">
-    .redhen-campaign-thermo-percent-bar-wrapper .redhen-campaign-thermo-percent-bar {background: red;}
-  </style>
-<?php else: ?>
-  <style type="text/css">
+    .custom-thermometer .redhen-campaign-thermo-percent-bar-wrapper .redhen-campaign-thermo-percent-bar {background: red;}
     .redhen-campaign-thermo-percent-<?php print $id; ?> { width: <?php print $percent; ?>%; }
   </style>
-<?php endif; ?>
 <div class="redhen-campaign-thermo-percent-bar-wrapper">
   <div class="redhen-campaign-thermo-percent-bar redhen-campaign-thermo-percent-<?php print $id; ?>"></div>
 </div>
